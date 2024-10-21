ef44c079f7304084d74cfee1cf2cdb0629d8cc14
Gowtham <sngowthamkumar081197@gmail.com>
2024-10-21 13:10:20 +0530

    OperatorValue

---
+++ src/September/Day1andDay2/class1.java
@@ -0,0 +1,27 @@
+package September.Day1andDay2;
+
+public class class1 {
+    public static void main(String[] args) {
+
+        System.out.println("from here the calculator opetions will start");
+
+        double a=34;
+        double b=10;
+        double sum=a+b;
+        double sub=a-b;
+        double multiplication=a*b;
+        double div=a/b;
+        double mod=a%b;
+
+        System.out.printf("the addition  value is %f", sum);
+        System.out.println();
+        System.out.printf("the Subtraction  value is %f", sub);
+        System.out.println();
+        System.out.printf("the multiplication  value is %f", multiplication);
+        System.out.println();
+        System.out.printf("the Division  value is %f", div);
+        System.out.println();
+        System.out.printf("the modulus  value is %f", mod);
+
+    }
+ }
